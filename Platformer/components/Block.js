cc353ce606866714845a8fcc2a766f3276274ba3
lightningV0LT <ethan.d.escorcia@gmail.com>
2025-11-13 17:57:44 -0600

    Update Block.js

--- Platformer/components/Block.js
+++ Platformer/components/Block.js
@@ -1,20 +1,30 @@
-import { AssetLoader } from './AssetLoader.js';
+import AssetLoader from './AssetLoader.js';
 import { aabb } from '../utils.js';
-/** Tile class **/
-export class Block {
-    static size = 100;
-    constructor(config) {
-        Object.assign(this, { w: Block.size, h: Block.size }, config);
+import behaviors from '../data/behavior-registry.js';
+
+export default class Block {
+  static size = 100;
+
+  constructor(config) {
+    Object.assign(this, {...config, w: Block.size * (config.w ?? 1), h: Block.size * (config.h ?? 1)});
+  }
+  
+  update({ players, game }) {
+    players.forEach(player => player.contactBlocks[aabb(player, this) ? "add" : "delete"](this));
+
+    if (this.action && typeof this.action === 'object') {
+      Object.entries(this.action).forEach(([k, factor]) => {
+        if (k in behaviors.action) behaviors.action[k](players, this, factor, game);
+      });
     }
-    update(players) {
-        players.forEach(player => player.contactBlocks[aabb(player, this) ? "add" : "delete"](this));
-        this.action?.(this, players);
-    }
-    display(cam) {
-        const view = cam.view(this);
-        if(!view) return;
-        const { x, y, w, h } = view;
-        this.visuals?.(this, x, y, w, h);
-        image(AssetLoader.blocks[this.tag] || AssetLoader.blocks.defBlock, x, y, w, h);
-    }
+  }
+
+  display(cam) {
+    const view = cam.view(this);
+    if(!view) return;
+    const { x, y, w, h } = view;
+    this.visuals?.(this, x, y, w, h);
+    
+    image(AssetLoader.blocks[this.tag] || AssetLoader.blocks.defBlock, x, y, w, h);
+  }
 }
