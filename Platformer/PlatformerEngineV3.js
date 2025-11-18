7fdfd0ae09fd13d33c3b2ee228151deb659f47bc
lightningV0LT <ethan.d.escorcia@gmail.com>
2025-11-17 21:51:02 -0600

    Add files via upload

---
+++ Platformer/PlatformerEngineV3.js
@@ -0,0 +1,81 @@
+import { SceneLoader, Transition } from './components/SceneSystem.js';
+import AssetLoader from './components/AssetLoader.js';
+import Player from './components/Player.js';
+import Game from './Game.js';
+
+let pressing = false;
+let released = false;
+let dragging = false;
+let blockKey;
+
+function preload() {
+  AssetLoader.block_sprites = loadImage('assets/block-sprites.png');
+  Game.level_data = loadJSON('data/level-data.JSON');
+  Game.block_data = loadJSON('data/block-data.JSON');
+  Player.stats = loadJSON('data/player-stats.JSON');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  globalThis.viewport = { x: 0, y: 0, w: windowWidth, h: windowHeight };
+
+  try {
+    SceneLoader.create({
+      name: 'loading',
+      run() {
+        AssetLoader.load();
+        frameRate(60);
+        Transition.restart(() => SceneLoader.swap('game'));
+      },
+    })
+    SceneLoader.create(new Game());
+    SceneLoader.create({
+      name: 'otherScene',
+      run() {
+        background(255, 0, 0);
+        if(keyIsDown(84)) Transition.restart(() => SceneLoader.swap('game'));
+      }
+    });
+  } catch(e) {
+    console.log(e);
+  }
+
+  SceneLoader.swap('loading');
+}
+
+function draw() {
+    background(255);
+    try {
+        SceneLoader.run();
+    } catch(e) {
+        console.error(e);
+    }
+
+    Transition.run();
+}
+
+window.preload = preload;
+window.setup = setup;
+window.draw = draw;
+
+function mousePressed() {
+    released = false;
+    pressing = true;
+}
+function mouseDragged() {
+    dragging = true;
+}
+function mouseReleased() {
+    pressing = false;
+    released = true;
+    dragging = false;
+}
+function mouseOut() {
+    pressing = false;
+    released = true;
+    dragging = false;
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
